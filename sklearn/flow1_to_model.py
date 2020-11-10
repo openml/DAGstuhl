51b1d03a7a15ff4094668230c081505dca776667
Prabhant Singh <prabhantsingh@gmail.com>
2020-11-10 12:21:38 +0100

    variable rename

--- sklearn/flow1_to_model.py
+++ sklearn/flow1_to_model.py
@@ -7,12 +7,13 @@ import requests
 import jsonschema
 import json
 
-
-flow_id = 1634700
+flow_id = 16347
 flow = openml.flows.get_flow(flow_id)
 s = openml.extensions.sklearn.extension.SklearnExtension()
 model = s.flow_to_model(flow)
 print(model)
+
 flow2 = to_flow(model)
-jsonschema.validate(flow, requests.get('https://openml.github.io/flow2/schemas/v0/pipeline.json').json())
-print(json.dumps(flow, allow_nan=False, ensure_ascii=False, indent=2))
+print(flow2)
+jsonschema.validate(flow2, requests.get('https://openml.github.io/flow2/schemas/v0/pipeline.json').json())
+print(json.dumps(flow2, allow_nan=False, ensure_ascii=False, indent=2))
